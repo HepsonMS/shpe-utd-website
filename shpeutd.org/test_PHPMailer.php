8d966d5412206017e04b52ff1e80177fd9147de6
Hepson Sanchez <hepsonms6@gmail.com>
2019-06-16 23:53:08 -0700

    file for testing whether PHPMailer is working

---
+++ shpeutd.org/test_PHPMailer.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This example shows settings to use when sending via Google's Gmail servers.
+ * This uses traditional id & password authentication - look at the gmail_xoauth.phps
+ * example to see how to use XOAUTH2.
+ * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
+ */
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 3;
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "utdshpe@gmail.com";
+//Password to use for SMTP authentication
+$mail->Password = "fpexxcenjhhzlbsd";
+//Set who the message is to be sent from
+$mail->setFrom('utdshpe@gmail.com');
+//Set who the message is to be sent to
+$mail->addAddress('hepsonms6@gmail.com');
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+//Replace the plain text body with one created manually
+$mail->Body = 'This is a plain-text message body';
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+    //Section 2: IMAP
+    //Uncomment these to save your message in the 'Sent Mail' folder.
+    #if (save_mail($mail)) {
+    #    echo "Message saved!";
+    #}
+}
