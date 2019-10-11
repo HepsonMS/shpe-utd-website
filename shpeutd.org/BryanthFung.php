68787ddf6885c0b529bd4ceae0c2302dfc73a82e
Bryanth <bryanthfung88@gmail.com>
2019-10-11 18:43:31 -0500

    PHP
    Changed to php

--- shpeutd.org/BryanthFung.php
+++ shpeutd.org/BryanthFung.php
@@ -1,6 +1,28 @@
 ﻿
 <!DOCTYPE HTML>
 <html>
+<?php
+$pageStart = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>MY WEBSITE PAGE</title>
+<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/jquery-ui.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(e) {
+    $("#date").datepicker();
+});
+</script>
+</head>
+<body>
+<input type="text" id="date" name="date" />
+</body>
+</html>';
+
+print $pageStart;
+?>
 	<head>
 		<title>Testing</title>
 		<meta charset="utf-8" />
@@ -63,10 +85,29 @@
 								<h2>&nbsp&nbspClick to Generate</h2>
 							</header>
 							<section id="two" class="spotlights">
+								<form> 
+								<input type="submit" value="generate" onCLick="generate()">
+								</form>
+								
 								<!-- PHP CODE GOES HERE -->
-                                <?php
-                                    echo (rand());
-                                ?>
+								<?php
+								if (isset($_POST['action'])) {
+									switch ($_POST['action']) {
+										case 'generate':
+											insert();
+											break;
+										
+									}
+								}
+								function generate(){
+									
+										   echo (rand());
+										   exit;
+                                	
+								}
+								?>
+								
+								
 							</section>
 						<!-- Three -->
 							<section id="three">
