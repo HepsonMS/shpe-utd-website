0f13e4e17a1d7190b0a43c3db5fe724480dde45d
Gabriel Flynn <gm.flynn99@yahoo.com>
2019-10-11 18:16:42 -0500

    Added random number generation

---
+++ shpeutd.org/GabrielFlynn.php
@@ -0,0 +1,134 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Testing</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<script type="text/javascript">
+			function Random() {
+				var a = Math.floor(1 + Math.random() * 1000)
+				document.getElementById('random').innerHTML = a;
+			}
+			
+		</script>
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+				<!-- Note: The "styleN" class below should match that of the banner element. -->
+					<header id="header" class="alt style2">
+						<a href="index.php" class="logo"><strong>SHPE</strong> <span>at UT Dallas</span></a>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<ul class="links">
+							<li><a href="index.php">Home</a></li>
+                            <li><a href="newsletter.php">Newsletter</a></li>
+							<li><a href="calendar.php">Calendar</a></li>
+							<li><a href="#contact">Contact</a></li>
+                                                        
+						</ul>
+					</nav>
+
+				<!-- Banner -->
+				<!-- Note: The "styleN" class below should match that of the header element. -->
+					<section id="banner" class="style2">
+						<div class="inner">
+							<span class="image">
+								<img src="images/pic07.jpg" alt="" />
+							</span>
+							<header class="major">
+								<h1>Random Number Generator</h1>
+							</header>
+							<div class="content">
+								
+							</div>
+						</div>
+					</section>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- One -->
+							
+
+						<!-- Two -->
+							<header>
+								<br>
+								<p id="random"></p>
+								<button onclick="Random()">Click to Generate with javascript</button>
+							</header>
+							<section id="two" class="spotlights">
+								<!-- PHP CODE GOES HERE -->
+								
+									<p><?php
+									echo rand(1,1000)
+									?></p>
+								
+							</section>
+						<!-- Three -->
+						<section id="three">
+								
+							</section>
+
+					</div>
+
+				<!-- Contact -->
+					<section id="contact">
+						<div class="inner">
+							<section>
+									<div class="contact-method">
+										<span class="icon alt fa-home"></span>
+										<h3>Address</h3>
+										<span>800 W Campbell Rd.<br />
+										Richardson, TX 75080<br />
+										United States of America</span>
+									</div>
+							</section>
+							<section class="split">
+								<section>
+									<div class="contact-method">
+										<span class="icon alt fa-envelope"></span>
+										<h3>Email</h3>
+										<a href="mailto:utdshpe@gmail.com" target="_blank">utdshpe@gmail.com</a>
+									</div>
+								</section>
+								<section>
+									<div class="icons">
+										<ul class="icons">
+											<li><a href="https://twitter.com/shpeutd?lang=en" class="icon alt fa-twitter" target="_blank"><span class="label" >Twitter</span></a></li>
+											<li><a href="https://www.facebook.com/SHPEUTD/" class="icon alt fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+											<li><a href="https://www.instagram.com/shpeutd/" class="icon alt fa-instagram" target="_blank"><span class="label">Instagram</span></a></li>
+										</ul>
+										<h3>Follow us on Social Media</h3>
+									</div>
+								</section>
+							</section>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
